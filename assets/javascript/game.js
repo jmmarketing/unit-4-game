bcfde68ea7e95065654deceb1dfb9ebdf65baf15
JeffM <jeff@jeffreymclean.com>
2019-09-02 14:26:36 -0700

    Added function to generate random target score, added function to randomly assign values to crystals. Need to add values to crystal images still

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -2,15 +2,47 @@
 
 //App/Page Loads 
 $(document).ready(function() {
+ 
+  
+    
+        
+        // List of Global Variables
 
-//     -A Random number is Generated for the computer (19 - 120)
-var computerScore = Math.floor(Math.random() * (120 - 19 + 1)) + 19;
-console.log(computerScore);
+        var computerScore;
+        var targetScore;
+        var crystalOne = [0,1,2,3,4,5,6,7,8,9,10,11,12];
+        var crystalTwo = [0,1,2,3,4,5,6,7,8,9,10,11,12];
+        var crystalThree = [0,1,2,3,4,5,6,7,8,9,10,11,12];
+        var crystalFour = [0,1,2,3,4,5,6,7,8,9,10,11,12];
+        
 
-var playerScore = 0;
+    //  Function that randomly assign number to computerScore Variable (19-120). 
+        function genCompScore (){
+            computerScore = Math.floor(Math.random() * (120 - 19 + 1)) + 19;
 
-//         - That number is assigned to a Variable
-//         - That number is show on the page
+            //Takes computerScore and shows on DOM
+            $("#targetScore").text(computerScore);
+        }
+
+
+function assignAllCrystals (){
+    // Function that Randomly Assigns number to Crystal 1
+            crystalOne = Math.floor(Math.random() * crystalOne.length);
+
+    // Function that Randomly Assigns number to Crystal 2
+            crystalTwo = Math.floor(Math.random() * crystalTwo.length);
+
+    // Function that Randomly Assigns number to Crystal 3
+            crystalThree = Math.floor(Math.random() * crystalThree.length);
+
+    // Function that Randomly Assigns number to Crystal 4
+            crystalFour = Math.floor(Math.random() * crystalFour.length);
+    }
+        
+        genCompScore();
+        assignAllCrystals();
+        console.log(crystalOne + " " + crystalTwo + " "  + crystalThree + " " + crystalFour);
+        console.log(computerScore);
 
 //     -Addtionally Four(4) Random Numbers are Generated (0 - 12) for the user 
 //         - Each generated number is assigned to separate variables (4)
